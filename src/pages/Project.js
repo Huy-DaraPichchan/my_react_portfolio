8a5104663b2740e8441fe0a3a6f396b863641b01
Huy-DaraPichchan <huy.darapichchan19@kit.edu.kh>
2023-06-28 12:47:53 +0700

    Finished building

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,13 +1,32 @@
 import React, { useState } from "react";
 import Header from "../components/Header.js";
 import Footer from "../components/Footer.js";
-import Avatar from "../assets/avatar.png";
-import { FaGithub, FaFacebook } from "react-icons/fa";
+import Avatar from "../assets/me.jpg";
+import { FaGithub, FaFacebook, FaTelegram } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 import { BsFillTelephoneFill } from "react-icons/bs";
+
 import OdooLogo from "../assets/odoo/odoo.png";
 import FlutterLogo from "../assets/flutter/flutter.png";
 import GodotLogo from "../assets/godot/godot.jpg";
+
+import GodotHomeScreen from "../assets/godot/homescreen.jpg";
+import GodotSetting from "../assets/godot/setting.jpg";
+import GodotGamePlay1 from "../assets/godot/gameplay1.jpg";
+import GodotGamePlay2 from "../assets/godot/gameplay2.jpg";
+import GodotGameOver from "../assets/godot/game_over.jpg";
+
+import FlutterOverview from "../assets/flutter/overview.jpg";
+import FlutterSIgnIn from "../assets/flutter/sign_in.jpg";
+
+import OdooRequestTree from "../assets/odoo/request_tree.jpg";
+import OdooRequestForm from "../assets/odoo/request_form.png";
+import OdooPOTree from "../assets/odoo/po_tree.jpg";
+import OdooPOForm from "../assets/odoo/po_form.jpg";
+import OdooSurveyTree from "../assets/odoo/survey_price_tree.jpg";
+import OdooSurveyForm from "../assets/odoo/survey_price_form.jpg";
+import OdooDetailReport from "../assets/odoo/detail_report.png";
+import OdooPaymentReport from "../assets/odoo/payment_report.png";
 
 const PortfolioHomePage = () => {
   const projects = [
@@ -16,21 +35,39 @@ const PortfolioHomePage = () => {
       title: "Odoo 13 System Development & Upgrade",
       description:
         "Involve in developing an application in the system which is used for make Purchase Request, Purchase Order, Create Report Summary, do Price Survey, and Show Report Analysis",
-      image: OdooLogo,
+      logo: OdooLogo,
+      detailImages: [
+        OdooRequestTree,
+        OdooRequestForm,
+        OdooPOTree,
+        OdooPOForm,
+        OdooSurveyTree,
+        OdooSurveyForm,
+        OdooDetailReport,
+        OdooPaymentReport,
+      ],
     },
     {
       id: 2,
       title: "Flutter App Development",
       description:
         "Built an application that generates cooking recipes for the user in there mobile phones",
-      image: FlutterLogo,
+      logo: FlutterLogo,
+      detailImages: [FlutterOverview, FlutterSIgnIn],
     },
     {
       id: 3,
       title: "Mobile Game Development",
       description:
         "Built a side-scrolling mobile game for Android devices using GoDot",
-      image: GodotLogo,
+      logo: GodotLogo,
+      detailImages: [
+        GodotHomeScreen,
+        GodotSetting,
+        GodotGamePlay1,
+        GodotGamePlay2,
+        GodotGameOver,
+      ],
     },
   ];
 
@@ -38,8 +75,8 @@ const PortfolioHomePage = () => {
     github: "https://github.com/Huy-DaraPichchan",
     cv: "https://drive.google.com/file/d/1IXkKXvhLwVt6coAfhHL7MAnTiBKl4dkI/view?usp=drive_link",
     linkedin: "https://linkedin.com/your-linkedin-profile",
-    facebook: "https://facebook.com/your-facebook-profile",
-    telegram: "https://t.me/your-telegram-profile",
+    facebook: "https://www.facebook.com/darapichchan.huy.9",
+    telegram: "https://t.me/Luizee444",
     phoneNumber: "tel:+85586418040",
   };
 
@@ -56,7 +93,7 @@ const PortfolioHomePage = () => {
   };
 
   return (
-    <div className='bg-gray-100 min-h-screen'>
+    <div className='bg-gray-100 min-h-screen font-mono'>
       <Header />
 
       <section id='hero' className='bg-gray-900 text-white py-16 px-8'>
@@ -74,7 +111,11 @@ const PortfolioHomePage = () => {
         <div className='container mx-auto flex flex-col items-center md:flex-row'>
           {/* Avatar */}
           <div className='flex-shrink-0 mb-8 md:mb-0 md:mr-8'>
-            <img className='w-32 h-32 sm:w-48 sm:h-48 md:w-64 md:h-64 rounded-full' src={Avatar} alt='Avatar' />
+            <img
+              className='w-32 h-32 sm:w-48 sm:h-48 md:w-64 md:h-64 rounded-full object-cover'
+              src={Avatar}
+              alt='Avatar'
+            />
           </div>
 
           {/* Description */}
@@ -92,14 +133,14 @@ const PortfolioHomePage = () => {
               have any inquiries or opportunities for collaboration. Thank you
               for your attention.
             </p>
-            <br/>
+            <br />
             <a
               href={socialMediaLinks.cv}
               className='bg-blue-500 hover:bg-blue-600 text-white px-5 py-3 rounded-lg text-lg font-semibold'
               target='_blank'
               rel='noopener noreferrer'
             >
-              Check My CV
+              Check Out My CV
             </a>
           </div>
         </div>
@@ -107,8 +148,8 @@ const PortfolioHomePage = () => {
 
       <section id='projects' className='py-16 px-8'>
         <div className='container mx-auto'>
-          <h2 className='text-3xl font-bold mb-8'>My Projects Summary</h2>
-          <div className='grid gap-8 text-center'>
+          <h2 className='text-3xl font-bold mb-8'>My Projects Overview</h2>
+          <div className='grid gap-8'>
             {projects.map((project) => (
               <div
                 key={project.id}
@@ -117,21 +158,15 @@ const PortfolioHomePage = () => {
                 <h3 className='text-2xl font-bold mb-4'>{project.title}</h3>
                 <div className='mb-4 flex justify-center'>
                   <img
-                    src={project.image}
+                    src={project.logo}
                     alt={project.title}
                     className='w-2/6 h-auto rounded-lg'
                   />
                 </div>
-                <p className='sm:mx-5 sm:text-sm md:mx-10 md:text-xl text-gray-700'>{project.description}</p>
+                <p className='sm:mx-15 sm:text-sm md:mx-25 md:text-xl text-gray-700 text-center'>
+                  {project.description}
+                </p>
                 <div className='flex justify-end mt-4'>
-                  {/* <a
-                    href={project.demoLink}
-                    target='_blank'
-                    rel='noopener noreferrer'
-                    className='text-blue-500 hover:underline'
-                  >
-                    View Detail
-                  </a> */}
                   <button
                     className='text-white hover:opacity-70 bg-blue-500 px-4 py-2 rounded-lg text-lg font-semibold'
                     onClick={() => handleProjectExpand(project.id)}
@@ -142,19 +177,17 @@ const PortfolioHomePage = () => {
                   </button>
                 </div>
                 {expandedProjects.includes(project.id) && (
-                  <div className='mt-4'>
+                  <div className='flex flex-col mt-4'>
                     {/* Add additional images here */}
-                    <img
-                      src={project.image2}
-                      alt={project.title}
-                      className='w-full h-auto rounded-lg'
-                    />
-                    <img
-                      src={project.image3}
-                      alt={project.title}
-                      className='w-full h-auto rounded-lg'
-                    />
-                    {/* ... Add more images as needed ... */}
+                    {project.detailImages.map((image, index) => (
+                      <div className='flex justify-center py-3' key={index}>
+                        <img
+                          src={image}
+                          alt={project.title}
+                          className='rounded-lg'
+                        />
+                      </div>
+                    ))}
                   </div>
                 )}
               </div>
@@ -197,14 +230,14 @@ const PortfolioHomePage = () => {
           >
             <FaFacebook size={28} />
           </a>
-          {/* <a
+          <a
             className='mr-4 text-sky-500 hover:opacity-70'
             href={socialMediaLinks.telegram}
             target='_blank'
             rel='noopener noreferrer'
           >
             <FaTelegram size={28} />
-          </a> */}
+          </a>
 
           <a
             href='mailto:huy.darapichchan19@kit.edu.kh'
